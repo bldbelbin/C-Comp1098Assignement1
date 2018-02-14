58ffadc2060ac8aa27021fe639907468cbfc2bd3
bldbelbin <34898179+bldbelbin@users.noreply.github.com>
2018-02-14 11:43:13 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace B.Belbin.Assignment1
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+        {
+            // Variables in Program.Main method
+
+            // define two variables that would represent the First Name and Last Name of a student also combined with assigning values of my first and last name
+            // to hold the value of my name
+            String FirstName = "Bethany";
+            String LastName = "Belbin";
+            String StudentName = FirstName + LastName;
+
+            // Integer variable to hold student number and define a variable that represents an unsigned number
+            int StudentNumber = 200232303;
+
+            /*
+            * Writes Student name on one line and writes student number on the next
+            * Readkey allows for the name and number to stay on the screen
+            */
+
+            Console.WriteLine(String.Format("Student Name {0}\nStudent Number {1}", StudentName, StudentNumber));
+
+            // initiate object Student
+            Student student = new Student();
+            student.AcceptDetails();
+
+            // variable that represents a string data type
+            String birthMonth = "April";
+
+            // define a variable that would represent the Age of a student
+            int ageOfStudent;
+
+            Console.WriteLine($"Birth Month: { birthMonth}");
+            Console.WriteLine(student.ToString());
+            Console.ReadKey(true);
+
+
+        }
+
+    }
+}
+
+
---
+++ Student.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace B.Belbin.Assignment1
+{
+    // define a Class that would represent a student
+    /// <summary>
+    /// To initiate an object as I did in Program.cs "Student student = new Student();"
+    /// create a default constructor. First is the name of the class followed by the object name. 
+    /// </summary>
+    class Student
+    {
+      
+            
+            // Setting properties with get and set allows for the values to be assigned as well as give program.cs access to return the value
+
+            // properties for the value FirstName
+            public string FirstName { get; set; }
+
+            // properties for the value LastName
+            public string LastName { get; set; }
+
+            // properties for the value Age
+            public int Age { get; set; }
+            public string Output { get; private set; }
+
+            // A method to output the results from program.cs
+            public void AcceptDetails()
+            {
+                FirstName = "Lauren";
+                LastName = "Belbin";
+                Age = 26;
+            }
+
+            // Returns a String that represents the Student object
+            public override string ToString()
+            {
+                Output = String.Format("First Name: {0}\nLast Name: {1}\nAge: {2}", FirstName, LastName, Age);
+                return Output;
+            }
+        }
+    }
+
+
